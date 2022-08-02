5d1e6defdebd9d3cc1b0b79c120a85f6af02ff0b
khaled39ab <khaled39ab@gmail.com>
2022-08-02 23:56:56 +0600

    change url

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -17,7 +17,7 @@ const Shop = () => {
          const storedCart = getStoredCart()
          const savedCart = [];
          for (const id in storedCart) {
-             const addedProduct = products.find(product => product.id === id);
+             const addedProduct = products.find(product => product._id === id);
              if (addedProduct) {
                  const quantity = storedCart[id];
                  addedProduct.quantity = quantity;
